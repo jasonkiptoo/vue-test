349ae60cb04f02aadb74916303f55d91eabb54a9
jasonkiptoo <kiptoojason@gmail.com>
2024-03-13 10:45:49 +0300

    new feature on routing

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -22,13 +22,28 @@
     width: 100%;
     background-color: grey;
     display: flex;
+    justify-content: center;
+    
 }
 .nav{
-    display: flex;
-    flex-direction: column;
-    float: right;
     list-style: none;
+    display: flex;
+    cursor: pointer;
+    justify-items: center;
+    margin: 10px;
+    /* display: flex;
+    flex-direction: row;
+   justify-content: space-between;
+    float: right;
+    background-color: rebeccapurple;
+    list-style: none; */
 
 text-decoration: none;
+}
+.nav li{
+    padding: 10px;
+}
+.nav li:hover{
+    color: white;
 }
 </style>
